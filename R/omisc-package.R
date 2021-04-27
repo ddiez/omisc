b034e1d3813326839bd0bfff94e171ea00b80318
Diego Diez <diego10ruiz@gmail.com>
2021-04-27 13:05:59 +0900

    Add dplyr dependency.

--- R/omisc-package.R
+++ R/omisc-package.R
@@ -4,6 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @import dplyr
 #' @importFrom grid grid.newpage grid.draw
 #' @importFrom VennDiagram venn.diagram
 #' @importFrom futile.logger flog.threshold
