9de7bfa5b2cd9ddb78c009513f5fa2a7b3349af7
Diego Diez <diego10ruiz@gmail.com>
2021-06-16 15:04:18 +0900

    Add to_tidy helper function to convert matrix and other objects into tidy data.frames.

--- R/misc.R
+++ R/misc.R
@@ -200,3 +200,53 @@ compute_pca.matrix <- function(x) {
   prcomp(dist(x), scale = TRUE)
 }
 
+#' to_tidy
+#'
+#' Convert a matrix, data.frame or tibble into a tidy tibble.
+#'
+#' @param x a matrix, data.frame or tibble object.
+#' @param row.name name for row data.
+#' @param col.name name for column data.
+#' @param value.name name use for value column.
+#' @param stringsAsFactors logical; whether to convert col/row names to factors (preserving original ordering).
+#' @param ... arguments passed to methods.
+#' @export
+to_tidy <- function(x, ...) {
+  UseMethod("to_tidy")
+}
+
+#' @rdname to_tidy
+#' @export
+to_tidy.matrix <- function(x, row.name = "row", ...) {
+  if (is.null(rownames(x)))
+    x <- as_tibble(x) %>% rownames_to_column(var = row.name)
+  else
+    x <- as_tibble(x, rownames = row.name)
+  to_tidy(x, row.name = row.name, ...)
+}
+
+#' @rdname to_tidy
+#' @export
+to_tidy.data.frame <- function(x, row.name = "row", ...) {
+  y <- as_tibble(x) %>% rownames_to_column(var = row.name)
+  to_tidy(y, row.name = row.name, ...)
+}
+
+
+#' @rdname to_tidy
+#' @export
+to_tidy.tbl_df <- function(x, row.name = "row", col.name = "col", value.name = "value", stringsAsFactors = FALSE, ...) {
+  y <- x %>% gather(!!col.name, !!value.name, -!!row.name, ...)
+
+  if (stringsAsFactors) {
+    rn <- x[[row.name]]
+    cn <- colnames(x)[-1]
+
+    y <- y %>%
+      mutate(!!row.name := factor(.data[[row.name]], levels = rn)) %>%
+      mutate(!!col.name := factor(.data[[col.name]], levels = cn))
+  }
+
+  y
+}
+
