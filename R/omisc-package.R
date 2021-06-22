ecc9ba87af27cf4788a54630457d473ebd432287
Diego Diez <diego10ruiz@gmail.com>
2021-06-22 17:57:36 +0900

    Rework dependencies to speed up loading time.

--- R/omisc-package.R
+++ R/omisc-package.R
@@ -4,10 +4,7 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
-#' @import dplyr
+#' @import dplyr ggplot2
 #' @importFrom grid grid.newpage grid.draw
-#' @importFrom VennDiagram venn.diagram
-#' @importFrom futile.logger flog.threshold
-#' @importFrom Gviz IdeogramTrack GenomeAxisTrack BiomartGeneRegionTrack DataTrack plotTracks chromosome
 ## usethis namespace: end
 NULL
