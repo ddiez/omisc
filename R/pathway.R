f2deefec2047b1a5aedb95e9831ff0183dac6cb2
Diego Diez <diego10ruiz@gmail.com>
2022-09-01 16:26:46 +0900

    Fix get_pathway_genes().

--- R/pathway.R
+++ R/pathway.R
@@ -5,10 +5,14 @@
 #' @export
 get_pathway_genes <- function(pathway) {
   genes <- KEGGREST::keggGet(pathway)[[1]][["GENE"]]
+  if (is.null(genes)) return(NULL)
+
+
   sel <- grepl(";", genes)
   genes <- data.frame(entrezgene = genes[!sel], description = genes[sel])
-  genes |>
+  genes <- genes |>
     mutate(symbol = sub(";.*", "", description)) |>
     mutate(description = sub(".*; ", "", description)) |>
     select(entrezgene, symbol, description)
+  return(genes)
 }
