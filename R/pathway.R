3bfce86fd487d95b4acf0561272f409f2e559a92
Diego Diez <diego10ruiz@gmail.com>
2022-09-26 00:27:11 +0900

    Revert to f2deefec2047b1a5aedb95e9831ff0183dac6cb2

--- R/pathway.R
+++ R/pathway.R
@@ -5,14 +5,10 @@
 #' @export
 get_pathway_genes <- function(pathway) {
   genes <- KEGGREST::keggGet(pathway)[[1]][["GENE"]]
-  if (is.null(genes)) return(NULL)
-
-
   sel <- grepl(";", genes)
   genes <- data.frame(entrezgene = genes[!sel], description = genes[sel])
-  genes <- genes |>
+  genes |>
     mutate(symbol = sub(";.*", "", description)) |>
     mutate(description = sub(".*; ", "", description)) |>
     select(entrezgene, symbol, description)
-  return(genes)
 }
